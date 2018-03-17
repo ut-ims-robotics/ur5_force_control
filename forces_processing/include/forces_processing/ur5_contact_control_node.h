7fee3d374367c62c08ff4fb5fd77b035e24c40e2
Martin Appo <martinappo@gmail.com>
2018-03-17 12:54:47 +0200

    Accept cartesian impedance service calls.

--- forces_processing/include/forces_processing/ur5_contact_control_node.h
+++ forces_processing/include/forces_processing/ur5_contact_control_node.h
@@ -3,10 +3,23 @@
 #include <ros/ros.h>
 #include <contact_control/contact_control.h>
 #include <geometry_msgs/WrenchStamped.h>
+#include "ur5_moveit/robot_mover.h"
+#include "cartesian_impedance_msgs/ConfigureForceControl.h"
 
 class Ur5ContactControlNode {
 private:
+    ros::NodeHandle nh_;
+    ContactControl contact_control_ = ContactControl();
+    std::vector<std::future<Contact::EndCondition>> pending_futures_;
+
+    bool configure_cartesian_impedance(
+        cartesian_impedance_msgs::ConfigureForceControl::Request &req,
+        cartesian_impedance_msgs::ConfigureForceControl::Response &res);
+
 public:
-    Ur5ContactControlNode() {};
+    Ur5ContactControlNode(ros::NodeHandle nh) {
+      this->nh_ = nh;
+    };
+
     void init();
 };
